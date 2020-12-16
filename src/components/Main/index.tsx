9e7923290985b4b7c86c0aa58f7a16cc088c36a5
Juliano Costa Silva <apfjuliano@gmail.com>
2020-12-15 21:48:27 -0300

    Module 1 - Part 1: Storybook withKnobs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Component'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Logo imagem de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Component
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para a tela com código"
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado JCS')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Component'
+    )}
+  />
+)
+
+export const Secundary: Story = () => (
+  <Main
+    title={text('Title', 'React Super Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Component'
+    )}
+  />
+)
